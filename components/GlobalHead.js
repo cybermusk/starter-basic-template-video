8a75cf40e98d457316fce8d92ad1189dbdf98f62
cybermusk <cybermusk@usf.edu>
2024-08-14 17:36:35 +0800

    Update GlobalHead.js

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -165,7 +165,7 @@ const GlobalHead = props => {
           __html: JSON.stringify({
             "@context": "https://schema.org",
             "@type": "WebSite",
-            "name": "Sugar Daddy Canada"
+            "name": "Sugar Daddy Meet NZ"
           })
         }}
       />
