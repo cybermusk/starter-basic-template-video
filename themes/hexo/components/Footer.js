aae4d89a45787b2525cbc73743fb1923cf736823
cybermusk <cybermusk@usf.edu>
2024-09-03 17:23:15 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -53,12 +53,12 @@ const Footer = ({ title }) => {
       </span>
 
       {/* Divider Line */}
-      <div className='border-t border-gray-300 dark:border-gray-600 my-8 mx-4'></div>
+      <div className='border-t border-gray-300 dark:border-gray-600 my-4 mx-4'></div>
 
-      <div className='flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-16 text-xs pt-4 text-light-500 dark:text-gray-500'>
+      <div className='flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-16 text-xs text-light-500 dark:text-gray-500'>
         {/* Column 1 */}
         <div className='flex flex-col text-left'>
-          <h4 className='font-bold mb-4'>OUR COMPANY</h4>
+          <h4 className='font-bold mb-2'>OUR COMPANY</h4>
           <a href='/company/about-us' className='hover:underline'>About Us</a>
           <a href='/support/feedbacks' className='hover:underline'>Contact Us</a>
           <a href='/support/faq' className='hover:underline'>Help and FAQ</a>
@@ -67,7 +67,7 @@ const Footer = ({ title }) => {
         
         {/* Column 2 */}
         <div className='flex flex-col text-left'>
-          <h4 className='font-bold mb-4'>SAFETY</h4>
+          <h4 className='font-bold mb-2'>SAFETY</h4>
           <a href='/safety/privacy-policy' className='hover:underline'>Privacy Policy</a>
           <a href='/safety/service-agreement' className='hover:underline'>Service Agreement</a>
           <a href='/safety/notice-of-collection' className='hover:underline'>Notice of Collection</a>
@@ -78,7 +78,7 @@ const Footer = ({ title }) => {
         
         {/* Column 3 */}
         <div className='flex flex-col text-left'>
-          <h4 className='font-bold mb-4'>OTHER DATING LOCATIONS</h4>
+          <h4 className='font-bold mb-2'>OTHER DATING LOCATIONS</h4>
           <a href='https://www.sugardaddysingapore.com' target='_blank' className='hover:underline'>Sugar Daddy Singapore</a>
           <a href='https://www.sugardaddycanada.com' target='_blank' className='hover:underline'>Sugar Daddy Canada</a>
           <a href='https://www.sugardaddyaustralia.com' target='_blank' className='hover:underline'>Sugar Daddy Australia</a>
@@ -90,13 +90,13 @@ const Footer = ({ title }) => {
             </>
           )}
           <button onClick={toggleExpand} className='pt-2 text-left text-light-500 dark:text-gray-500'>
-            {isExpanded ? '<<Less' : 'More>>'}
+            {isExpanded ? '<< Less' : 'More >>'}
           </button>
         </div>
         
         {/* Column 4 */}
         <div className='flex flex-col text-left'>
-          <h4 className='font-bold mb-4'>DOWNLOAD APP</h4>
+          <h4 className='font-bold mb-2'>DOWNLOAD APP</h4>
           <div className='flex justify-start mt-2'>
             <a href='https://www.sugardaddymeet.com/registerStep1?tid=af100028782_qqx' target='_blank' rel='noopener noreferrer'>
               <img src='https://raw.githubusercontent.com/icon-other/icon/main/download/App%20Store.png' alt='Download By App Store' className='w-24 h-auto mx-2' />
