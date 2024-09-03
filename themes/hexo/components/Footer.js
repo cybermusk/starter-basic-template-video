13a81fb72b4aac6f3d055b4cc723db66d6d02579
cybermusk <cybermusk@usf.edu>
2024-09-03 15:12:08 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -6,49 +6,66 @@ const Footer = ({ title }) => {
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
-return (
-  <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
-    {/* <DarkModeButton/> */}
-    <i className='fas fa-copyright' /> {`${copyrightDate}`}
-    <span>
-      <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-      <a href={siteConfig('LINK')} className='underline font-bold dark:text-gray-300'>
-        {siteConfig('AUTHOR')}
-      </a>
-      .<br />
-      {siteConfig('BEI_AN') && (
-        <>
-          <i className='fas fa-shield-alt' />{' '}
-          <a href='https://beian.miit.gov.cn/' className='mr-2'>
-            {siteConfig('BEI_AN')}
-          </a>
-          <br />
-        </>
-      )}
-      <span className='hidden busuanzi_container_site_pv'>
-        <i className='fas fa-eye' />
-        <span className='px-1 busuanzi_value_site_pv'> </span>
-      </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-      </span>
-      <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>
-        {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
-      </p>
-      <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
-        Copyright © 2007-2024{' '}
-        <a href='https://www.sugardaddymeet-nz.com' className='dark:text-gray-300'>
-          sugardaddymeet-nz.com
+  return (
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
+      {/* <DarkModeButton/> */}
+      <i className='fas fa-copyright' /> {`${copyrightDate}`}
+      <span>
+        <i className='mx-1 animate-pulse fas fa-heart' />{' '}
+        <a href={siteConfig('LINK')} className='underline font-bold dark:text-gray-300'>
+          {siteConfig('AUTHOR')}
         </a>
-        , INC. All rights reserved.
-      </p>
-      <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
-        Disclaimer: You must be over 19 to join. SugarDaddyMeet-NZ is not an escort service. SugarDaddyMeet-NZ does not support escorts or prostitution. Profiles that may violate these terms and services will be banned.
-      </p>
-    </span>
-  </footer>
-);
-
+        .<br />
+        {siteConfig('BEI_AN') && (
+          <>
+            <i className='fas fa-shield-alt' />{' '}
+            <a href='https://beian.miit.gov.cn/' className='mr-2'>
+              {siteConfig('BEI_AN')}
+            </a>
+            <br />
+          </>
+        )}
+        <span className='hidden busuanzi_container_site_pv'>
+          <i className='fas fa-eye' />
+          <span className='px-1 busuanzi_value_site_pv'> </span>
+        </span>
+        <span className='pl-2 hidden busuanzi_container_site_uv'>
+          <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
+        </span>
+        <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>
+          {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
+        </p>
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
+          Copyright © 2007-2024{' '}
+          <a href='https://www.sugardaddymeet-nz.com' className='dark:text-gray-300'>
+            sugardaddymeet-nz.com
+          </a>
+          , INC. All rights reserved.
+        </p>
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
+          Disclaimer: You must be over 19 to join. SugarDaddyMeet-NZ is not an escort service. SugarDaddyMeet-NZ does not support escorts or prostitution. Profiles that may violate these terms and services will be banned.
+        </p>
+      </span>
+      <div className='flex justify-between text-xs pt-4 text-light-500 dark:text-gray-500'>
+        <div className='flex flex-col'>
+          <h4 className='font-bold'>OUR COMPANY</h4>
+          <a href='/company/about-us' className='underline'>About Us</a>
+          <a href='/support/feedbacks' className='underline'>Contact Us</a>
+          <a href='/support/faq' className='underline'>Help and FAQ</a>
+          <a href='/Become-an-Affiliate' className='underline'>Become an Affiliate</a>
+        </div>
+        <div className='flex flex-col'>
+          <h4 className='font-bold'>SAFETY</h4>
+          <a href='/safety/privacy-policy' className='underline'>Privacy Policy</a>
+          <a href='/safety/service-agreement' className='underline'>Service Agreement</a>
+          <a href='/safety/notice-of-collection' className='underline'>Notice of Collection</a>
+          <a href='/safety/community-guidelines' className='underline'>Community Guidelines</a>
+          <a href='/safety/fighting-human-trafficking' className='underline'>Stop Sex & Human Trafficking</a>
+          <a href='/safety/no-sell-or-share' className='underline'>Do Not Sell or Share My Personal Information</a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
