7d56d2823b5da249bbbaae7913869ed098233a95
cybermusk <cybermusk@usf.edu>
2024-09-24 17:36:46 +0800

    Update MenuItem.js

--- themes/starter/components/MenuItem.js
+++ themes/starter/components/MenuItem.js
@@ -13,7 +13,7 @@ export const MenuItem = ({ link }) => {
             href={link?.href}
             target={link?.target}
           {/* className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${router.route === '/' ? 'lg:text-white lg:group-hover:text-white' : ''} lg:group-hover:opacity-70`}> */}
-            className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${router.route === '/' ? 'lg:text-white lg:group-hover:text-white' : ''} lg:group-hover:opacity-70`}>
+            className={`ud-menu-scroll mx-8 flex py-2 text-base font-medium text-dark group-hover:text-[#cda780] dark:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${router.route === '/' ? 'lg:text-white lg:group-hover:text-white' : ''}`}>
             {link?.icon && <i className={link.icon + ' mr-2 my-auto'} />}
             {link?.name}
           </Link>
