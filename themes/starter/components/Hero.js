445c1b79917bb9cdfa91c476c0e0c4350e033f1a
cybermusk <cybermusk@usf.edu>
2024-09-26 15:51:00 +0800

    Update Hero.js

--- themes/starter/components/Hero.js
+++ themes/starter/components/Hero.js
@@ -16,7 +16,7 @@ export const Hero = () => {
           loop
           muted
         >
-          <source src="/video/starter/custom-video/test1.mp4" type="video/mp4" />
+          <source src="/videos/starter/custom-videos/test1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
