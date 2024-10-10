a2faf3596caa87705d2e0cfc53c404002cbaff56
cybermusk <cybermusk@usf.edu>
2024-10-10 10:52:09 +0800

    Update SocialButton.js

--- themes/fukasawa/components/SocialButton.js
+++ themes/fukasawa/components/SocialButton.js
@@ -13,7 +13,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_GITHUB') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'github'}
             href={siteConfig('CONTACT_GITHUB')}>
             <i className='fab fa-github transform hover:scale-125 duration-150' />
@@ -22,7 +22,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_TWITTER') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'twitter'}
             href={siteConfig('CONTACT_TWITTER')}>
             <i className='fab fa-twitter transform hover:scale-125 duration-150' />
@@ -31,7 +31,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_TELEGRAM') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             href={siteConfig('CONTACT_TELEGRAM')}
             title={'telegram'}>
             <i className='fab fa-telegram transform hover:scale-125 duration-150' />
@@ -40,7 +40,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_LINKEDIN') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             href={siteConfig('CONTACT_LINKEDIN')}
             title={'linkedIn'}>
             <i className='fab fa-linkedin transform hover:scale-125 duration-150' />
@@ -49,7 +49,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_WEIBO') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'weibo'}
             href={siteConfig('CONTACT_WEIBO')}>
             <i className='fab fa-weibo transform hover:scale-125 duration-150' />
@@ -58,7 +58,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_INSTAGRAM') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'instagram'}
             href={siteConfig('CONTACT_INSTAGRAM')}>
             <i className='fab fa-instagram transform hover:scale-125 duration-150' />
@@ -67,7 +67,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_EMAIL') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'email'}
             href={`mailto:${siteConfig('CONTACT_EMAIL')}`}>
             <i className='fas fa-envelope transform hover:scale-125 duration-150' />
@@ -76,7 +76,7 @@ const SocialButton = () => {
         {JSON.parse(siteConfig('ENABLE_RSS')) && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'RSS'}
             href={'/rss/feed.xml'}>
             <i className='fas fa-rss transform hover:scale-125 duration-150' />
@@ -85,7 +85,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_BILIBILI') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'bilibili'}
             href={siteConfig('CONTACT_BILIBILI')}>
             <i className='fab fa-bilibili transform hover:scale-125 duration-150' />
@@ -94,7 +94,7 @@ const SocialButton = () => {
         {siteConfig('CONTACT_YOUTUBE') && (
           <a
             target='_blank'
-            rel='noreferrer'
+            rel='noreferrer nofollow'
             title={'youtube'}
             href={siteConfig('CONTACT_YOUTUBE')}>
             <i className='fab fa-youtube transform hover:scale-125 duration-150' />
